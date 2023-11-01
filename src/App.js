deb0bb4bc2d1385e08152a410cd76173aa29f601
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-11-01 00:25:22 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './modules/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,18 +1,39 @@
-import React from 'react'
-import Hero from '../../components/Hero'
-import Products from '../../components/Products'
-import FeatureCard from '../../components/FeatureCard'
+import React, { useEffect, useState } from "react";
+import Hero from "../../components/Hero";
+import Products from "../../components/Products";
+import FeatureCard from "../../components/FeatureCard";
+import StatCard from "../../components/StatCard";
 
 export default function Home() {
+
+const [products, setProducts] = useState([])  
+
+useEffect(()=>{
+  async function fetchProducts(){
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data =await response.json()
+    console.log(data)
+    setProducts(data)
+  }
+  fetchProducts()
+},[])
+
+
+
   return (
     <>
-        <Hero />
-        <div className="flex flex-col text-center w-full mt-20">
-      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
-      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MOST POPULAR PRODUCTS</h1>
-    </div>
-        <Products />
-        <FeatureCard />
+      <Hero />
+      <div className="flex flex-col text-center w-full mt-20">
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MOST POPULAR PRODUCTS</h1>
+      </div>
+      {
+        products.length > 0 ? <Products products={products} />: <div>Loading.....</div>
+      }
+        
+      <Products />
+      <FeatureCard />
+      <StatCard />
     </>
-  )
+  );
 }
